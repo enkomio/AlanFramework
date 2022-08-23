9ac5926188ae9a78d640a63d8fc16b172a785024
piedacoulisse2 <74360897+piedacoulisse2@users.noreply.github.com>
2022-08-23 18:34:18 +0200

    Update Program.fs
    Modify the Console WriteLine to add Release

--- src/server/ServerBuilder/Program.fs
+++ src/server/ServerBuilder/Program.fs
@@ -341,5 +341,5 @@ module Program =
         let releaseDir = Path.Combine(workingDir, "..", "..", "..", "Misc", "out") |> Path.GetFullPath
         generateFakeContext(workingDir)
         build(workingDir, releaseDir)
-        Console.WriteLine("Release file written at: " + Path.Combine(releaseDir + Path.DirectorySeparatorChar.ToString(), releaseFilename))
+        Console.WriteLine("Release file written at: " + Path.Combine(releaseDir + Path.DirectorySeparatorChar.ToString() + "Release" + Path.DirectorySeparatorChar.ToString(), releaseFilename))
         0
